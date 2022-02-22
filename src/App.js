1917a2ca42915facbf0af9790f6bf52a8fa6f4c1
uhs <binarybard@naver.com>
2022-02-22 17:51:55 +0900

    school-work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
-import MainPageComponent from './main/index.js';
+import "./App.css";
+import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
   return (
-    <MainPageComponent />    
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />;
+        </Route>
+        <Route exact={true} path="/product">
+          {/* {} 생략 가능 */}
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
